4b627d4cdec2fab9b4fe148e156b57b8f72b5717
s.kryvets <s.kryvets@gmail.com>
2020-04-04 19:43:33 -0500

    Remove commented out code

--- auth-server/src/main/java/com/skryvets/JwkSetEndpoint.java
+++ auth-server/src/main/java/com/skryvets/JwkSetEndpoint.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.security.KeyPair;
-import java.security.Principal;
 import java.security.interfaces.RSAPublicKey;
 import java.util.Map;
 
@@ -21,7 +20,7 @@ class JwkSetEndpoint {
 
     @GetMapping("/.well-known/jwks.json")
     @ResponseBody
-    public Map<String, Object> getKey(Principal principal) {
+    public Map<String, Object> getKey() {
         RSAPublicKey publicKey = (RSAPublicKey) this.keyPair.getPublic();
         RSAKey key = new RSAKey.Builder(publicKey).build();
         return new JWKSet(key).toJSONObject();
